9ec8da6a36b2427224a9c94f7ba5bfcb6800909b
ja3cob <jakubecker@gmail.com>
2024-06-23 22:51:28 +0200

    Add value to actionresult

--- ASPClientLib/Classes/ActionResult.cs
+++ ASPClientLib/Classes/ActionResult.cs
@@ -3,4 +3,8 @@
 namespace ASPClientLib.Classes;
 
 public class ActionResult : IActionResult;
-public class ActionResult<T>;
+
+public class ActionResult<T>(T value)
+{
+    public T Value { get; } = value;
+}
